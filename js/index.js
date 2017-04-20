00dab82b2ae3a9951a5741ea729e0a243a1b205a
Chengcheng Pei <chengcheng.pei@esentire.com>
2017-04-20 11:31:55 -0400

    post working, simple initial html layout and css

--- js/index.js
+++ js/index.js
@@ -1,16 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var testButton = document.getElementById('btn');
-  testButton.addEventListener('click', function() {
-    document.getElementById('txt').innerHTML = 'Text changed.';
-    /*
-    chrome.tabs.getSelected(null, function(tab) {
-      d = document;
-      var para = document.createElement("p");
-      var node = document.createTextNode("This is new.");
-      para.appendChild(node);
-      d.body.appendChild(para);
-      f.submit();
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('DOMContentLoaded.');
+  
+  /* Get URL of active tab. */
+  $(document).ready( () => {
+    const queryInfo = {
+      active: true,
+      currentWindow: true
+    }
+
+    chrome.tabs.query( queryInfo, tabs => {
+      const tab = tabs[0];
+      const url = tab.url;
+      /* DOM operations */
+      document.getElementById('urlInput').value = url;
     });
-    */ 
+  });
+
+  /* When ENTER key down, hidden input and show code. */
+  $(document).on('keydown', e => {
+    const txt = $('textarea#codesInput').val().trim();
+    if(e.which === 13 && txt) {
+      $('pre#pre').removeClass('hidden');
+      $('code#code').text(txt);
+      $('textarea#codesInput').addClass('hidden');
+    }    
+  });
+
+  /* When code dbclicked, hidden code and show input. */
+  $('pre#pre').on('dblclick', e => {
+    $('pre#pre').addClass('hidden');
+    $('textarea#codesInput').removeClass('hidden');
+  });
+
+  /* When button clicked, SUBMIT/POST data to server. */
+  $('#submit').on('click', function() {
+    const paras = {
+      'url': $('#urlInput').val(),
+      'comments': $('#commentsInput').val(),
+      'codes': $('textarea#codesInput').val()
+    }
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:8000/',
+      data: paras,
+      success: function(d) {
+        console.log(JSON.stringify(d));
+      },
+      error: function(e) {
+        alert(JSON.stringify(e));
+      }
+    });
   });
 }, false);
